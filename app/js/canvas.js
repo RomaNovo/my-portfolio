650543d7b493fb89429163b631a7bda5170805c3
RomaNovo <roma_1985_roma@mail.ru>
2018-03-10 12:55:13 +0200

    canvas production version

---
+++ app/js/canvas.js
@@ -0,0 +1,65 @@
+
+var colCanvas    = _qA('.my_canvas'),
+ 	arrCanvas    = [];
+ 	skills 	     = _q('#skills'),
+ 	heightSkills = skills.offsetTop,
+ 	scroll       = window.pageYOffset,
+ 	count        = 0;
+
+function showCanvas() {
+	colCanvas.forEach(function(v,i) {
+		var progress = v.dataset.progress;
+		arrCanvas[i] = v.getContext('2d');
+		drawCanvas.call(null, arrCanvas[i], progress);
+	})
+}
+
+function drawCanvas(context,persent) {
+	var al = 0,
+		start = 4.72,
+	    cw = context.canvas.width,
+		ch = context.canvas.height,
+		diff;
+
+	function progressSim() {
+
+		diff = ((al / 100) * Math.PI*2*10).toFixed(2);
+		context.clearRect(0, 0, cw, ch);
+		context.lineWidth   = 10;
+		context.fillStyle   = '#2c3e50'; ;
+		context.strokeStyle = "#2c3e50";	
+		context.textAlign   = 'center';
+		context.font        = "13px tahoma";
+		context.fillText(al + '%', cw*.5, ch*.5+2, cw);
+		context.beginPath();
+		context.arc(60,60,55, start, diff/10+start, false);				
+		context.stroke();
+
+		context.beginPath();
+		context.arc(60,60,55,4.72,3.5*Math.PI);
+		context.globalCompositeOperation = 'destination-over';
+		context.lineWidth   = 10;
+		context.strokeStyle = 'white';	
+		context.stroke();
+
+		if (al >= persent) {
+				clearTimeout(sim);
+			}
+			al++;
+		}
+		var sim = setInterval(progressSim, 15);
+	}
+
+if (scroll + 200 > heightSkills) {
+	showCanvas();
+	count++;
+}
+
+window.addEventListener('scroll', function() {
+ 	var scrol = window.pageYOffset;
+	if (count > 0) return;
+	if (scrol + 500 > heightSkills) {
+		showCanvas();
+		count++;
+	}	
+})
--- app/js/main.js
+++ app/js/main.js
@@ -30,39 +30,11 @@ burger.addEventListener('click', function() {
 });
 
 window.onload = function() {
-/* ============== FORM =================*/
-
- /*formName.addEventListener('input', function() {
- 	if(this.value.length>0){
- 		labelText.style.opacity = '1';
- 	} else {
- 		labelText.style.opacity = '0';
- 	}
- })
-
- formEmail.addEventListener('input', function() {
- 	if(this.value.length>0){
- 		labelEmail.style.opacity = '1';
- 	} else {
- 		labelEmail.style.opacity = '0';
- 	}
- })
-
- formArea.addEventListener('input', function() {
- 	if(this.value.length>0){
- 		labelArea.style.opacity = '1';
- 	} else {
- 		labelArea.style.opacity = '0';
- 	}
- })*/
 
 
- 
 
-/* ============== FORM-END =============*/
+// ====== Logo ======
 
-
-// ====== Triangle ======
 var logo = _q('.logo');
 
 function showLogo() {
@@ -70,93 +42,9 @@ function showLogo() {
 }
 setTimeout(showLogo, 2000);
 
-//====== CANVAS =======
+// ====== LogoEnd ======
 
-	var ctx1 = _q('.my_canvas1').getContext('2d'),
-		ctx2 = _q('.my_canvas2').getContext('2d'),
-		ctx3 = _q('.my_canvas3').getContext('2d'),
-		ctx4 = _q('.my_canvas4').getContext('2d'),
-		ctx5 = _q('.my_canvas5').getContext('2d'),
-		ctx6 = _q('.my_canvas6').getContext('2d'),
-		ctx7 = _q('.my_canvas7').getContext('2d'),
-		ctx8 = _q('.my_canvas8').getContext('2d'),
-		ctx9 = _q('.my_canvas9').getContext('2d');
-			
-	function test(obj,persent) {
-		var al = 0;
-		var start = 4.72;
-		var cw = obj.canvas.width;
-		var ch = obj.canvas.height;
-		var diff;
-		function progressSim() {
-			
-			diff = ((al / 100) * Math.PI*2*10).toFixed(2);
-			obj.clearRect(0, 0, cw, ch);
-			obj.lineWidth = 10;
-			obj.fillStyle = '#2c3e50'; // persent color;
-			obj.strokeStyle = "#2c3e50";	
-			obj.textAlign = 'center';
-			obj.fillText(al + '%', cw*.5, ch*.5+2, cw);
-			obj.beginPath();
-
-			obj.arc(60,60,55, start, diff/10+start, false);
-			
-			
-				
-			obj.stroke();	// вывод на экран
-			if(al >= persent) {
-				clearTimeout(sim);
-			}
-			al++;
-		}
-		var sim = setInterval(progressSim, 15);
-	}
-
-var skills = _q('#skills');
-
-var heightSkills = skills.offsetTop;
-
-var scrol = window.pageYOffset;
-var count = 0;	
-	if(scrol + 200 > heightSkills) {
-	test(ctx1,65);
-	test(ctx2,80);	
-	test(ctx3,85);
-	test(ctx4,90);
-	test(ctx5,100);
-	test(ctx6,75);
-	test(ctx7,85);
-	test(ctx8,90);	
-	test(ctx9,99);
-	count++;
-	}
-
-
-window.addEventListener('scroll', function() {
-	var scrol = window.pageYOffset;
-	if(count > 0) return;
-	if(scrol + 200 > heightSkills) {
-	test(ctx1,65);
-	test(ctx2,80);	
-	test(ctx3,85);
-	test(ctx4,90);
-	test(ctx5,100);
-	test(ctx6,75);
-	test(ctx7,85);
-	test(ctx8,90);	
-	test(ctx9,99);
-	count++;
-	}	
-})
-
-
-		
-
-	
-
-	// ======== CANVAS END ==================
-
-	// ======== SCROOL CONTROL ==============
+// ======== SCROOL CONTROL ==============
 
 	var navMenu = _q('.nav'),
 		aboutD  = _q('.about__descr'),
